be247c5ebbc44bd801c8e6a03f779c9a76f61fbc
Minh Phat <phattm204@gmail.com>
2020-08-07 21:28:06 +0700

    Add checkbox type for toggle

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import React from "react";
 const Todo = ({ toggleTodo, deleteTodo, completed, text }) => (
   <li className={completed ? "completed" : ""}>
     <div className="view">
-      <input className="toggle" onClick={toggleTodo} />
+      <input className="toggle" type="checkbox" onClick={toggleTodo} />
       <label>{text}</label>
       <button className="destroy" onClick={deleteTodo}></button>
     </div>
